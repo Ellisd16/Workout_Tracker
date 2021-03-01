1e9454f351064d15d6f9f0cc9ffbd39e585f4168
Ellis Delgado <ellisdelgado13@gmail.com>
2021-03-01 15:31:33 -0800

    Added a gitignore file, made intial changes to workout.js, deleted exercise.js

---
+++ Develop/models/workout.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//we'll export this workout schema when we're done!
+const workoutSchema = new Schema({
+    //here well establish the exercises needed for the workout schema
+
+    //So, in the mongodb we need a row for date & for exercises
+
+})
