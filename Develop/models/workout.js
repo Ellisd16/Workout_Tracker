88cbc5468f934e5c4d645ec18ac7fe7c4a0426d8
Ellis Delgado <ellisdelgado13@gmail.com>
2021-03-01 15:43:40 -0800

    Added schema types to workout.js

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -8,4 +8,39 @@ const workoutSchema = new Schema({
 
     //So, in the mongodb we need a row for date & for exercises
 
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercise: [{
+        //for exercise, we need type, name, duration, weight, reps, and sets
+
+        type: {
+            type: String,
+            trim: true,
+            required: "Please select a type"
+        },
+        name: {
+            type: "String",
+            trim: true,
+            required: "Enter a name"
+        },
+        duration: {
+            type: Number,
+            required: "Enter an amount"
+        },
+        weight: {
+            type: Number,
+            required: "Enter an amount"
+        },
+        reps: {
+            type: Number,
+            required: "Enter an amount"
+        },
+        sets: {
+            type: Number,
+            required: "Enter an amount"
+        }
+    }
+    ]
 })
