d0fdf2c210dc39d7e19eb2f8b8a22d31d976e76c
Ellis Delgado <ellisdelgado13@gmail.com>
2021-03-01 16:25:06 -0800

    Added a route to api-routes, changed the server the app ran on

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
 
-const Workout = require("../models");
+const db = require("../models");
 
 
 //i'm gonna need the urls "/api/workouts/", "/api/workouts/range"
-router.post("/api/workouts/", ({ body }, res) => {
-    Workout.create(body)
-        .then(dbWorkout)
+
+//
+router.post("/api/workouts/", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        });
 })
+
+//exporting the api routes
+
+module.exports = router
