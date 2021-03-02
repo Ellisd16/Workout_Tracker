34853c0ce13098ce9046027c5cb4add7e286bae2
Ellis Delgado <ellisdelgado13@gmail.com>
2021-03-01 16:01:15 -0800

    Adding code to server.js and to api-routes

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
 
 const Workout = require("../models");
+
+
+//i'm gonna need the urls "/api/workouts/", "/api/workouts/range"
+router.post("/api/workouts/", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout)
+})
